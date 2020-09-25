52939df91efeafc8a2e7c0739c42a10d3bb5665d
Krzysztof Kisiel <qminec@gmail.com>
2020-09-25 13:43:40 +0200

    Dicegame 1.0

--- src/spil/Dice.java
+++ src/spil/Dice.java
@@ -1,5 +1,26 @@
 package spil;
 
+import java.util.Random;
+
 public class Dice {
+
+    private static int faceValue;
+    static int max = 6;
+    private static int sum;
+
+    public Dice(int faceValue, int max) {
+        Dice.faceValue = faceValue;
+        Dice.max = max;
+    }
+
+    public static int roll(){
+        Random r = new Random();
+        faceValue = r.nextInt(max)+1;
+        return faceValue;
+    }
+
+   public String toString(){
+       return Integer.toString(faceValue);
+   }
 
 }
--- src/spil/Main.java
+++ src/spil/Main.java
@@ -3,5 +3,11 @@ package spil;
 public class Main {
     public static void main(String[] args) {
 
+        for (int i = 0; i < 10; i++) {
+            Dice.roll();
+            System.out.println("Roll:" + (i+1) + "  " + Dice.roll());
+        }
+
+
     }
 }
