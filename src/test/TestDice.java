c6b3d4952a4573404dacf8bc4b916a8fd82c4354
Oliver-Olsen <oliver@skou-olsen.dk>
2020-09-26 18:00:33 +0200

    Test

--- src/test/TestDice.java
+++ src/test/TestDice.java
@@ -1,4 +1,31 @@
 package test;
 
+import java.util.Random;
+
 public class TestDice {
+
+    //Max and min i defined
+    private static final int Max_dots = 6;
+    private static final int min = 1;
+
+    public static void main(String[] args) {
+
+
+        //"Random" class is implemented
+        Random die = new Random();
+
+        //Random number is generated
+        // int die1 = die.nextInt(Max_dots)+min;
+
+
+        //Loop 1000 times
+        for (int i = 0; i < 1000; i++) {
+
+            //Random number is generated
+            int die1 = die.nextInt(Max_dots) + min;
+
+            System.out.println(die1);
+
+        }
+    }
 }
