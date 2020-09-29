f49a6266fd095990ea3c646f7a6132339a2dac7a
Oliver-Olsen <oliver@skou-olsen.dk>
2020-09-29 20:52:07 +0200

    Fordeling af kast

--- src/test/TestDice.java
+++ src/test/TestDice.java
@@ -1,40 +1,37 @@
 package test;
-
-import spil.Dice;
-
 import java.util.Random;
+import java.util.Scanner;
 
-public class TestDice extends Dice {
-
-    //Max and min i defined
-    private static final int Max_dots = 6;
-    private static final int min = 1;
-
-    public TestDice(int dice1, int dice2) {
-        super(dice1, dice2);
-    }
-
-
-
+public class TestDice {
 
     public static void main(String[] args) {
 
+        int rollCount;
+        int die;
 
-        //"Random" class is implemented
-        Random die = new Random();
+        System.out.println("Inddtast antal terning-kast");
 
-        //Random number is generated
-        // int die1 = die.nextInt(Max_dots)+min;
+        Scanner input = new Scanner(System.in);
+        rollCount = input.nextInt();
 
 
-        //Loop 1000 times
-        for (int i = 0; i < 1000; i++) {
 
-            //Random number is generated
-            int die1 = die.nextInt(Max_dots) + min;
 
-            System.out.println(die1);
+        int[] diceCount ={0,0,0,0,0,0};
 
+        Random r = new Random();
+
+        //Loop x times
+        for (int i = 0; i < rollCount; i++) {
+
+            die = r.nextInt(6)+1;
+            diceCount[die-1]++;
+
+        }
+        System.out.println("Fordeling at kast:");
+        for (int i=0;i<6;i++){
+
+            System.out.println(i+1 + ": " + diceCount[i]);
         }
     }
 }
