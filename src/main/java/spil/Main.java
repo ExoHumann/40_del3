12f1865cacd3f56b060a1879e870d6320afae4c4
Krzysztof Kisiel <qminec@gmail.com>
2020-09-30 02:03:05 +0200

    Converted project into Added Maven__diplomitdtu_matadorgui_3_1_7.xml

--- src/main/java/spil/Main.java
+++ src/main/java/spil/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        GUI gui = new GUI();
+        //GUI gui = new GUI();
         Scanner in = new Scanner(System.in);
 
         System.out.println("Set your name");
