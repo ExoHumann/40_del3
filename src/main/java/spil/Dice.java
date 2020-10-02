21792304fb15c5328eeea291aeaa458466128074
Oliver-Olsen <oliver@skou-olsen.dk>
2020-10-02 15:07:54 +0200

    Descriptions added

--- src/main/java/spil/Dice.java
+++ src/main/java/spil/Dice.java
@@ -8,6 +8,7 @@ public class Dice {
     private int dice2;
 
 
+    //Creates two random dice and gets the sum of them
     public Dice(int dice1, int dice2) {
         this.dice1 = dice1;
         this.dice2 = dice2;
@@ -15,6 +16,7 @@ public class Dice {
     }
 
 
+    //Method for getting the sum
     public int roll(){
         Random r = new Random();
         dice1 = r.nextInt(6)+1;
@@ -22,6 +24,8 @@ public class Dice {
         return getSum();
     }
 
+
+    //Methods/requirements
     boolean getEquals(){ return dice1 == dice2; }
     public void setDice (int dice1, int dice2) { this.dice1 = dice1; this.dice2 = dice2;}
     public int getDice1() { return dice1; }
