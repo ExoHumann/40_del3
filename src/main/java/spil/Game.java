7d5f69eca91fb0f1c594e4de4cb6f91d41a9e76a
Krzysztof Kisiel <qminec@gmail.com>
2020-10-06 15:20:22 +0200

    framework

--- src/main/java/spil/Game.java
+++ src/main/java/spil/Game.java
@@ -1,25 +1,18 @@
 package spil;
 
-import gui_fields.GUI_Field;
-import gui_main.GUI;
-
 public class Game {
     private final Player player1;
     private final Player player2;
     private final int gamesAmount;
     private final Dice cDice;
     private final Dice pDice;
-    static GUI gameGUI;
-    private final GUI_Field[] fields;
 
-    public Game(GUI gameGUI, Player player1, Player player2, int games, Dice cDice, Dice pDice){
+    public Game(Player player1, Player player2, int games, Dice cDice, Dice pDice){
         this.player1 = player1;
         this.player2 = player2;
         this.cDice = cDice;
         this.pDice = pDice;
         gamesAmount = games;
-        Game.gameGUI = gameGUI;
-        this.fields = gameGUI.getFields();
 
 
         play();
@@ -37,80 +30,28 @@ public class Game {
 
     private void play() {
 
+        for (int i = 0; i < gamesAmount; i++) {
+
         while (player1.getScore() < 40 && player2.getScore() < 40) {
 
-            movesPlayer(player1, pDice);
+            GameGUI.movesPlayer(player1, pDice);
             diceInfo(player1, pDice);
-            movesPlayer(player2, cDice);
+            GameGUI.movesPlayer(player2, cDice);
             diceInfo(player2, cDice);
 
-            if (pDice.getEquals()) {
-                if (pDice.getDice1() == 1) {
-                    movesToStart(player1);
-                } else if (pDice.getDice1() == 6) {
-                    movesPlayer(player1, pDice);
-                    diceInfo(player1, pDice);
-                    if (pDice.getEquals()) {
-                        if (pDice.getDice1() == 6) {
-                            player1.setScore(40);
-                            diceInfo(player1, pDice);
-                        }
-                    }
-                }
-            }
-            if (cDice.getEquals()) {
-                if (cDice.getDice1() == 1) {
-                    movesToStart(player2);
-                } else if (cDice.getDice1() == 6) {
-                    movesPlayer(player2, cDice);
-                    diceInfo(player2, cDice);
-                    if (cDice.getEquals()) {
-                        if (cDice.getDice1() == 6) {
-                            player2.setScore(40);
-                            diceInfo(player2, cDice);
-                        }
-                    }
-                }
-            }
         }
+
         if (player1.getScore() > player2.getScore()) {
-            player2.setWin();
-        } else if (player2.getScore() > player2.getScore()) {
             player1.setWin();
+        } else if (player2.getScore() > player1.getScore()) {
+            player2.setWin();
         } else if (player1.getScore() == player2.getScore()) {
             player1.setTie();
             player2.setTie();
         }
 
-        movesToStart(player1);
-        movesToStart(player2);
-    }
-
-    public void movesPlayer(Player player, Dice dice) {
-        int prePos = player.getCurrentPosition();
-        player.setBalance(prePos);
-        gameGUI.showMessage("Roll The Dice: " + player.getName() + "'s Turn");
-        player.move(dice.roll());
-        player.setScore(player.getScore() + dice.getSum());
-
-        int pos = player.getCurrentPosition();
-        if (fields[prePos].hasCar(player)) {
-            gameGUI.setDice(dice.getDice1(), dice.getDice2());
-            fields[prePos].setCar(player, false);
-            fields[pos].setCar(player, true);
-            player.setBalance(pos);
+        GameGUI.movesToStart(player1);
+        GameGUI.movesToStart(player2);
         }
     }
-
-    private void movesToStart(Player player) {
-        int prePos = player.getCurrentPosition();
-        if (fields[prePos].hasCar(player)) {
-            fields[prePos].setCar(player, false);
-            fields[0].setCar(player, true);
-            player.setBalance(0);
-            player.setCurrentPosition(0);
-            player.setScore(0);
-        }
-    }
-
 }
--- src/main/java/spil/GameGUI.java
+++ src/main/java/spil/GameGUI.java
@@ -10,12 +10,7 @@ public class GameGUI {
     static GUI_Field[] fields;
     static Player player1;
     static Player player2;
-    static public boolean hasReachedGoalP1 =false;
-    static public boolean hasReachedGoalP2 =false;
 
-    /**
-     * Creates and returns GUI object with 2 players
-     */
     public GameGUI(){
         fields = new GUI_Field[40];
 
@@ -32,8 +27,8 @@ public class GameGUI {
         GameGUI.gui = new GUI(fields,Color.cyan);
 
     }
-    //Players are added to the game and they are placed on the board
 
+    //Players are added to the game and they are placed on the board
     public void AddPlayers(Player player1, Player player2){
         GameGUI.player1 = player1;
         GameGUI.player2 = player2;
@@ -42,140 +37,34 @@ public class GameGUI {
         fields[0].setCar(player1, true);
         fields[0].setCar(player2, true);
     }
+    //Moves the player around the board
+    public static void movesPlayer(Player player, Dice dice) {
+        int prePos = player.getCurrentPosition();
+        player.setBalance(prePos);
+        gui.showMessage("Roll The Dice: " + player.getName() + "'s Turn");
+        player.move(dice.roll());
+        player.setScore(player.getScore() + dice.getSum());
+
+        int pos = player.getCurrentPosition();
+        if (fields[prePos].hasCar(player)) {
+            gui.setDice(dice.getDice1(), dice.getDice2());
+            fields[prePos].setCar(player, false);
+            fields[pos].setCar(player, true);
+            player.setBalance(pos);
+        }
+    }
+
+    public static void movesToStart(Player player) {
+        int prePos = player.getCurrentPosition();
+        if (fields[prePos].hasCar(player)) {
+            fields[prePos].setCar(player, false);
+            fields[0].setCar(player, true);
+            player.setBalance(0);
+            player.setCurrentPosition(0);
+            player.setScore(0);
+        }
+    }
 
     public static String GetPlayerName(){ return gui.getUserString("Write player name: "); }
 
-    /**
-     * Asks the selected player to roll the dice
-     * @param player true for player 1, false for player 2
-     * @return wether or not to roll the dice.
-     */
-    public static boolean rollDiceAction(Player player){
-        return gui.getUserLeftButtonPressed(player.getName()+", do you want to roll dice?","Yes","No");
-
-    }
-
-    /**
-     * Method to show dice on the gui
-     * @param dice1 number on the first dice
-     * @param dice2 number on the second dice
-     */
-    public static void showDice(int dice1, int dice2){
-        gui.setDice(dice1,dice2);
-    }
-
-    /**
-     * Method to update the selected players point total
-     * @param player player object to set points for
-     * @param pointsToAdd number of points to add to the players total
-     */
-    public static void movePlayer(Player player, int pointsToAdd){
-        if (player == player1){
-            if(!hasReachedGoalP1){
-                int field = getCurrentField(player1);
-                //Check if the other player is on the same field
-                if (fields[field].hasCar(player2)){
-                    fields[field].removeAllCars(); //Remove all the cars from the field
-                    fields[field].setCar(player2,true); //Add the second player back onto the field
-                }
-                else{
-                    fields[field].removeAllCars(); //Remove all the cars from the field
-                }
-                if(field+pointsToAdd>=40){
-                    fields[39].setCar(player1,true);
-                    player1.setBalance(40);
-                    hasReachedGoalP1 = true;
-                }else{
-                    fields[(field+pointsToAdd)-1].setCar(player1,true); //Update the score tracker of player 1
-                    player1.setBalance(field+pointsToAdd); //Update the score balance of player 1
-                    hasReachedGoalP1 = false;
-                }
-            }else{
-                fields[39].setCar(player2,true);
-            }
-
-
-        }
-        else if(player == player2){
-            if(!hasReachedGoalP2){
-                int field = getCurrentField(player2);
-                if (fields[field].hasCar(player1)){
-                    fields[field].removeAllCars();
-                    fields[field].setCar(player1,true);
-                }
-                else{
-                    fields[field].removeAllCars();
-                }
-                if(field+pointsToAdd>=40){
-                    fields[39].setCar(player2,true);
-                    player2.setBalance(40);
-                    hasReachedGoalP2 = true;
-                }else{
-                    fields[(field+pointsToAdd)-1].setCar(player2,true); //Update the score tracker of player 2
-                    player2.setBalance(field+pointsToAdd); //Update the score balance of player 2
-                    hasReachedGoalP2 = false;
-                }
-            }else{
-                fields[39].setCar(player2,true);
-            }
-
-        }
-    }
-
-    /**
-     * Returns the field number of where the selected player is located on the board.
-     * @param player The player to look for on the board
-     * @return Current field number
-     */
-    private static int getCurrentField(Player player){
-        int currentField = 0;
-
-        //Checks all tiles in the field array, for the selected player.
-        for(int i =0;i<40;i++){
-            if(fields[i].hasCar(player)){
-                currentField = i; //If the player has been found, save the current field number, and break
-                break;
-            }
-        }
-        return currentField;
-    }
-
-    /**
-     * Reset the selected players points
-     * @param player true for player 1, false for player 2
-     */
-    public static void moveToStart(Player player){
-        if(player == player1){
-            int field = getCurrentField(player1);
-            if (fields[field].hasCar(player2)){
-                fields[field].removeAllCars();
-                fields[field].setCar(player2,true);
-            }
-            else{
-                fields[field].removeAllCars();
-            }
-            fields[0].setCar(player1,true);
-            player1.setBalance(0);
-        }
-        else if(player == player2){
-            int field = getCurrentField(player2);
-            if (fields[field].hasCar(player1)){
-                fields[field].removeAllCars();
-                fields[field].setCar(player1,true);
-            }
-            else{
-                fields[field].removeAllCars();
-            }
-            fields[0].setCar(player2,true);
-            player2.setBalance(0);
-        }
-    }
-
-    /**
-     * Shows game end message, the selected player wins
-     * @param player true for player 1, false for player 2
-     */
-    public static void gameEnd(Player player){
-        gui.showMessage(player.getName()+" You Win!");
-    }
 }
