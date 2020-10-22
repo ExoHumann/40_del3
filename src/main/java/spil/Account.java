ba0bce2cd258da70f4146be80082d92ea50256cc
Jonathan Hansen <jonathan2xh@gmail.com>
2020-10-22 09:25:45 +0200

    Began work on account class

---
+++ src/main/java/spil/Account.java
@@ -0,0 +1,15 @@
+package spil;
+
+public class Account {
+
+    private int balance;
+
+    public Account(int newBalance) {
+        balance = newBalance;
+    }
+
+    public int getBalance() {
+        return balance;
+
+    }
+}
