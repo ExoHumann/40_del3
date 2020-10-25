ec7057ce185d8a27ec788dd30b232463bb982690
alaahadood <usahadood@gmail.com>
2020-10-25 12:54:44 +0100

    Account class

--- src/main/java/spil/Account.java
+++ src/main/java/spil/Account.java
@@ -1,14 +1,39 @@
 package spil;
 
-public class Account {
+    public class Account {
 
-    private int balance;
+        private int balance;
 
-    public Account(int balance) {
-        this.balance = this.balance;
+        public Account(int balance) {
+            this.balance = balance;
+        }
+
+        public void withdraw(int amount) {
+            balance = balance - amount;
+            if (balance<0) {
+                balance = 0;
+            }
+        }
+
+        public void deposit(int amount) {
+            balance = balance + amount;
+            if (balance<0) {
+                balance = 0;
+            }
+        }
+
+        public int getBalance() {
+            return balance;
+        }
+
+        public void setBalance(int balance) {
+            this.balance = balance;
+        }
+        public void addBalance(int amount){
+            balance = balance + amount;
+            if (balance<0) {
+                balance = 0;
+
+            }
+        }
     }
-
-    public int getBalance() { return balance; }
-    public void setBalance(int balance) { this.balance = balance; }
-
-}
