3853f4bdacf46e726a961134f51a9a6e653fb4b7
Joha6210 <johannesjojojensen@gmail.com>
2020-10-26 15:15:54 +0100

    Added AccountTest

---
+++ src/test/java/spil/AccountTest.java
@@ -0,0 +1,16 @@
+package spil;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @Test
+    void withdraw() {
+    }
+
+    @Test
+    void deposit() {
+    }
+}
