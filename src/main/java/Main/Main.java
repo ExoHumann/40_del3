491dfdf39e409971839f8211e7fedaa3483adc04
Krzysztof Kisiel <qminec@gmail.com>
2020-10-29 13:05:46 +0100

    sorted classes after the CVM method

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,8 +2,6 @@ package Main;
 
 import Controller.Game;
 
-import static gui_tests.TestRunExampleGame.sleep;
-
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
