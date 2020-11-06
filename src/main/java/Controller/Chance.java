8d053adac977edf58bb84ec0a4d852cee345dfdd
UnknownBrain <gufa1353@gmail.com>
2020-11-06 07:56:05 +0100

    Shuffle mechanism imported

--- src/main/java/Controller/Chance.java
+++ src/main/java/Controller/Chance.java
@@ -5,6 +5,8 @@ import Model.PlayerList;
 import Model.Playerlist.Account;
 import Model.Playerlist.Player;
 
+import java.util.Collections;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Chance {
@@ -14,13 +16,24 @@ public class Chance {
     }
 
     int chance;
+    private static final int deckSize=20;
 
     public void drawCard(){
         int amountOfCards = 20;
         Random r = new Random(amountOfCards);
         chance = r.nextInt();
+
     }
 
+    public static void main(String[] args) {
+        ArrayList<Integer> deck=new ArrayList<>();
+        for (int i = 0; i < deckSize; i++) {
+            deck.add(i);
+        }
+        Collections.shuffle(deck);
+        System.out.println(deck);
+
+    }
     public void chance(PlayerList pl, FieldList fl, int playerTurn, int playerChoice){
         Player player = pl.getPlayerList(playerTurn);
         Account account = pl.getAccount(playerTurn);
