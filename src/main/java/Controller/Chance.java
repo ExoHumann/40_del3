7a89f1d9f081279ebf1a6136330272cd40c33407
UnknownBrain <gufa1353@gmail.com>
2020-11-09 16:03:55 +0100

    Choose between fields cases

--- src/main/java/Controller/Chance.java
+++ src/main/java/Controller/Chance.java
@@ -41,10 +41,11 @@ public class Chance {
     public void chance(PlayerList pl, FieldList fl, int playerTurn, Logic logic, GameGUI gameGUI) throws InterruptedException {
         Player player = pl.getPlayerList(playerTurn);
         Account account = pl.getAccount(playerTurn);
-        int playerChoice = 0;
+        int playerChoice;
         drawCard();
         Random r = new Random();
-        chance = r.nextInt((3)+1);
+        chance = r.nextInt((12)+1);
+
 
         switch (chance) {
             case 0:
@@ -66,25 +67,94 @@ public class Chance {
                 gameGUI.buyField(pl,playerChoice,playerTurn);
                 break;
             case 4:
+               playerChoice = gameGUI.getUserButtons("Move one field or draw another card", 1,2);
                 if (playerChoice == 1){
-                    player.move(1,fl);
+                    logic.movePlayer(pl,fl, playerChoice, playerTurn);
+                    gameGUI.fancyMoveGuiPlayer(logic.prePos,playerTurn, playerChoice);
                 } else if (playerChoice == 2) {
-                    drawCard();
+                   drawCard();
                 }
+                break;
             case 5:
+                playerChoice = gameGUI.getUserButtons("Move to a light blue field", 16,17);
+                logic.movePlayer(pl,fl,fl.getSize() + playerChoice - logic.pos,playerTurn);
+                gameGUI.moveToField(logic.prePos,playerTurn,playerChoice);
+                gameGUI.buyField(pl,playerChoice,playerTurn);
+                break;
             case 6:
+                playerChoice = gameGUI.getUserButtons("Move to an orange field", 13,14);
+                logic.movePlayer(pl,fl,fl.getSize() + playerChoice - logic.pos,playerTurn);
+                gameGUI.moveToField(logic.prePos,playerTurn,playerChoice);
+                gameGUI.buyField(pl,playerChoice,playerTurn);
+                break;
             case 7:
+                playerChoice = gameGUI.getUserButtons("Move to a red field", 1,2);
+                logic.movePlayer(pl,fl,fl.getSize() + playerChoice - logic.pos,playerTurn);
+                gameGUI.moveToField(logic.prePos,playerTurn,playerChoice);
+                gameGUI.buyField(pl,playerChoice,playerTurn);
+                break;
             case 8:
+                playerChoice = gameGUI.getUserButtons("Move to (1) an orange field or (2) a green field",1,2);
+                if (playerChoice==1){
+                    playerChoice = gameGUI.getUserButtons("Choose an orange field", 13,14);
+                    logic.movePlayer(pl,fl,fl.getSize() + playerChoice - logic.pos,playerTurn);
+                    gameGUI.moveToField(logic.prePos,playerTurn,playerChoice);
+                    gameGUI.buyField(pl,playerChoice,playerTurn);}
+                    else if(playerChoice==2){
+                    playerChoice = gameGUI.getUserButtons("Choose a green field", 22,23);
+                    logic.movePlayer(pl,fl,fl.getSize() + playerChoice - logic.pos,playerTurn);
+                    gameGUI.moveToField(logic.prePos,playerTurn,playerChoice);
+                    gameGUI.buyField(pl,playerChoice,playerTurn);}
+                break;
             case 9:
                 player.setGetOutOfJailCard();
+                break;
             case 10:
+                playerChoice = gameGUI.getUserButtons("Move to (1) a pink field or (2) a dark blue field",1,2);
+                if (playerChoice==1){
+                    playerChoice = gameGUI.getUserButtons("Choose a pink field", 19,20);
+                    logic.movePlayer(pl,fl,fl.getSize() + playerChoice - logic.pos,playerTurn);
+                    gameGUI.moveToField(logic.prePos,playerTurn,playerChoice);
+                    gameGUI.buyField(pl,playerChoice,playerTurn);}
+                else if(playerChoice==2){
+                    playerChoice = gameGUI.getUserButtons("Choose a dark blue field", 10,11);
+                    logic.movePlayer(pl,fl,fl.getSize() + playerChoice - logic.pos,playerTurn);
+                    gameGUI.moveToField(logic.prePos,playerTurn,playerChoice);
+                    gameGUI.buyField(pl,playerChoice,playerTurn);}
+                break;
             case 11:
+                playerChoice = gameGUI.getUserButtons("Move to (1) a light blue field or (2) a red field",1,2);
+                if (playerChoice==1){
+                    playerChoice = gameGUI.getUserButtons("Choose a light blue field", 16,17);
+                    logic.movePlayer(pl,fl,fl.getSize() + playerChoice - logic.pos,playerTurn);
+                    gameGUI.moveToField(logic.prePos,playerTurn,playerChoice);
+                    gameGUI.buyField(pl,playerChoice,playerTurn);}
+                else if(playerChoice==2){
+                    playerChoice = gameGUI.getUserButtons("Choose a red field", 1,2);
+                    logic.movePlayer(pl,fl,fl.getSize() + playerChoice - logic.pos,playerTurn);
+                    gameGUI.moveToField(logic.prePos,playerTurn,playerChoice);
+                    gameGUI.buyField(pl,playerChoice,playerTurn);}
+                break;
             case 12:
+                playerChoice = gameGUI.getUserButtons("Move to (1) a dark green field or (2) a yellow field",1,2);
+                if (playerChoice==1){
+                    playerChoice = gameGUI.getUserButtons("Choose a dark green field", 7,8);
+                    logic.movePlayer(pl,fl,fl.getSize() + playerChoice - logic.pos,playerTurn);
+                    gameGUI.moveToField(logic.prePos,playerTurn,playerChoice);
+                    gameGUI.buyField(pl,playerChoice,playerTurn);}
+                else if(playerChoice==2){
+                    playerChoice = gameGUI.getUserButtons("Choose a yellow field", 4,5);
+                    logic.movePlayer(pl,fl,fl.getSize() + playerChoice - logic.pos,playerTurn);
+                    gameGUI.moveToField(logic.prePos,playerTurn,playerChoice);
+                    gameGUI.buyField(pl,playerChoice,playerTurn);}
+                break;
             case 13:
-                for (int i = 0; i < pl.getPlayerAmount(); i++) {
+                gameGUI.showMessage("It's your birthday! Every player gives you 1M");
+              for (int i = 0; i < pl.getPlayerAmount(); i++) {
                     pl.getAccount(pl.getPlayerAmount()).withdraw(1);
-                    account.deposit(pl.getPlayerAmount());
-                }
+                    account.deposit(pl.getPlayerAmount());}
+                    break;
+
             case 14:
             case 15:
             case 16:
